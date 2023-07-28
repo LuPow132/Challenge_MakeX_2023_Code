181f013c041a55aab55b842d4438e8d777d77407
LuPow132 <68886157+LuPow132@users.noreply.github.com>
2023-07-28 09:54:14 +0700

    Create power_manage_module.py

---
+++ INAD/Sample_Code/power_manage_module.py
@@ -0,0 +1,9 @@
+import novapi
+from mbuild import power_manage_module
+
+def start_board_with_power_management():
+        if power_manage_module.is_auto_mode():
+    
+            #auto code here
+        else:
+            #manual code here
